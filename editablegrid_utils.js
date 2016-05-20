3fde282b499f221c41d62c439d4df2d596e58bbe
myName <user@example.com>
2016-05-20 22:02:53 +0800

    fix number format

--- editablegrid_utils.js
+++ editablegrid_utils.js
@@ -320,6 +320,7 @@ String.prototype.endsWith = function(str) { return (this.match(str+"$")==str); }
  */
 EditableGrid.prototype.checkDate = function(strDate, strDatestyle) 
 {
+	// TODO: strptime()
 	strDatestyle = strDatestyle || this.dateFormat;
 	strDatestyle = strDatestyle || "EU";
 
@@ -752,25 +753,26 @@ function number_format (number, decimals, dec_point, thousands_sep) {
 	// *    example 13: number_format('1 000,50', 2, '.', ' ');
 	// *    returns 13: '100 050.00'
 	// Strip all characters but numerical ones.
+
 	number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
 	var n = !isFinite(+number) ? 0 : +number,
-			prec = !isFinite(+decimals) ? 0 : /*Math.abs(*/decimals/*)*/,
-					sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
-							dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
-									s = '',
-									toFixedFix = function (n, prec) {
-								var k = Math.pow(10, prec);
-								return '' + Math.round(n * k) / k;
-							};
-							// Fix for IE parseFloat(0.55).toFixed(0) = 0;
-							s = (prec < 0 ? ('' + n) : (prec ? toFixedFix(n, prec) : '' + Math.round(n))).split('.');
-							if (s[0].length > 3) {
-								s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
-							}
-							if ((s[1] || '').length < prec) {
-								s[1] = s[1] || '';
-								s[1] += new Array(prec - s[1].length + 1).join('0');
-							}
-							return s.join(dec);
+		prec = !isFinite(+decimals) ? 0 : /*Math.abs(*/decimals/*)*/,
+		sep = (typeof thousands_sep === 'undefined') ? '' : thousands_sep,
+		dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+		s = '',
+		toFixedFix = function (n, prec) {
+			var k = Math.pow(10, prec);
+			return '' + Math.round(n * k) / k;
+		};
+	// Fix for IE parseFloat(0.55).toFixed(0) = 0;
+	s = (prec < 0 ? ('' + n) : (prec ? toFixedFix(n, prec) : '' + Math.round(n))).split('.');
+	if (s[0].length > 3) {
+		s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+	}
+	if ((s[1] || '').length < prec) {
+		s[1] = s[1] || '';
+		s[1] += new Array(prec - s[1].length + 1).join('0');
+	}
+	return s.join(dec);
 }
 
